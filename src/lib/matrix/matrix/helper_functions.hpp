1bcdb80d5248e81e64d333502eea5ea0bf449da5
Maggie Wang <maggi3wang@gmail.com>
2016-08-05 13:38:04 -0700

    Finished most of traj feedback controller, in process of phasing out Matrix library and using Math library instead.

--- src/lib/matrix/matrix/helper_functions.hpp
+++ src/lib/matrix/matrix/helper_functions.hpp
@@ -16,25 +16,25 @@ namespace matrix
 template<typename Type>
 Type wrap_pi(Type x)
 {
-#if defined (__PX4_NUTTX) || defined (__PX4_QURT)
-    if (!isfinite(x)) {
-#else
-    if (!std::isfinite(x)) {
-#endif
-        return x;
-    }
+//#if defined (__PX4_NUTTX) || defined (__PX4_QURT)     // commented out by Maggie
+//    if (!isfinite(x)) {
+//#else
+//    if (!std::isfinite(x)) {
+//#endif
+//        return x;
+//    }
 
-    while (x >= (Type)M_PI) {
-        x -= (Type)(2.0 * M_PI);
+//    while (x >= (Type)M_PI) {
+//        x -= (Type)(2.0 * M_PI);
 
-    }
+//    }
 
-    while (x < (Type)(-M_PI)) {
-        x += (Type)(2.0 * M_PI);
+//    while (x < (Type)(-M_PI)) {
+//        x += (Type)(2.0 * M_PI);
 
-    }
+//    }
 
-    return x;
+//    return x;
 }
 
 
